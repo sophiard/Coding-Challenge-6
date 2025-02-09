58ae850852808a638f7af3a3b3c78abe1bb7a80c
Sophia Dolan <25srdolan@gmail.com>
2025-02-09 15:41:17 -0500

    Task 2 - Sales Tax Computation

--- cc_6.js
+++ cc_6.js
@@ -9,3 +9,14 @@ console.log("Task 1 profit 1:", calculateProfit(20, 30, 100));
 console.log("Task 1 Profit 2:", calculateProfit(50, 70, 200));
 //logged the profit 
 
+
+
+//Task 2 - Sales Tax Computation
+
+function calculateSalesTax(amount, taxRate) {
+    return(amount * taxRate)};
+//made a function to calculate sales tax
+
+console.log(`Task 2 Sales tax 1: $${Math.floor(calculateSalesTax(100, 0.07))}`); 
+console.log(`Task 2 Sales tax 1: $${Math.floor(calculateSalesTax(500, 0.1))}`);
+//logged both sale tax (used math floor to make it whole numbers)
