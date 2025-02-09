e6d180ff11f6928fe4c36713f05023a4df2ab2a7
Sophia Dolan <25srdolan@gmail.com>
2025-02-09 15:42:42 -0500

    Edited task 1: Made the log a template literal

--- cc_6.js
+++ cc_6.js
@@ -5,8 +5,8 @@ function calculateProfit(costPrice, sellingPrice, unitsSold) {
 }
 // I made a function to calculate profit
 // The directiosn say the formula to divide it but I think to find profit its multiplying
-console.log("Task 1 profit 1:", calculateProfit(20, 30, 100)); 
-console.log("Task 1 Profit 2:", calculateProfit(50, 70, 200));
+console.log(`Task 1 profit 1: $${calculateProfit(20, 30, 100)}`);
+console.log(`Task 1 profit 2: $${calculateProfit(50, 70, 200)}`);
 //logged the profit 
 
 
